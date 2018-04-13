418fbae28c1d323f7b145521a0f7708314a7a8e4
Killian Talin <killian.talin@live.fr>
2018-04-13 09:51:55 +0200

    ADD static file for all pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,29 @@ class PagesController < ApplicationController
 
   def marketing
   end
+
+  def administrative
+  end
+
+  def courage
+  end
+
+  def design
+  end
+
+  def formation
+  end
+
+  def gestion_interne
+  end
+
+  def inspiration_graphique
+  end
+
+  def recruter
+  end
+
+  def website
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,13 @@ Rails.application.routes.draw do
 
   get 'manifesto', to: 'pages#manifesto', as: :manifesto
   get 'marketing', to: 'pages#marketing', as: :marketing
+  get 'administrative', to: 'pages#administrative', as: :administratif
+  get 'courage', to: 'pages#courage', as: :courage
+  get 'design', to: 'pages#design', as: :design
+  get 'formation', to: 'pages#formation', as: :formation
+  get 'gestion_interne', to: 'pages#gestion_interne', as: :gestion_interne
+  get 'inspiration_graphique', to: 'pages#inspiration_graphique', as: :inspiration_graphique
+  get 'recruter', to: 'pages#recruter', as: :recruter
+  get 'website', to: 'pages#website', as: :website
+
 end
